159bece3d5533e436a6de46c5b7a4a5aa91d9144
lucasdavid <lucasolivdavid@gmail.com>
2018-03-15 19:06:27 -0300

    update strategies

--- connoisseur/fusion/strategies.py
+++ connoisseur/fusion/strategies.py
@@ -7,16 +7,16 @@ __all__ = AVAILABLE_STRATEGIES
 
 def sum(labels, distances, multi_class=True, t=0.0):
     if multi_class:
-        return np.argmax(distances.sum(axis=-2), axis=-1)
+        return np.asarray([np.argmax(d.sum(axis=-2), axis=-1) for d in distances])
 
-    return (distances.sum(axis=-1) > t).astype(np.int)
+    return np.asarray([(d.sum(axis=-1) > t) for d in distances], dtype=int)
 
 
 def mean(labels, distances, multi_class=True, t=0.0):
     if multi_class:
-        return np.argmax(distances.mean(axis=-2), axis=-1)
+        return np.asarray([np.argmax(d.mean(axis=-2), axis=-1) for d in distances])
 
-    return (distances.mean(axis=-1) > t).astype(np.int)
+    return np.asarray([(d.mean(axis=-1) > t) for d in distances], dtype=int)
 
 
 def contrastive_mean(labels, distances, multi_class=True, t=0.0):
@@ -25,13 +25,10 @@ def contrastive_mean(labels, distances, multi_class=True, t=0.0):
 
 def farthest(labels, distances, multi_class=True, t=0.0):
     if multi_class:
-        return np.argmax(distances.max(axis=-2), axis=-1)
+        return np.asarray([np.argmax(d.max(axis=-2), axis=-1) for d in distances])
 
-    n_samples = distances.shape[0]
-    patches = np.argmax(np.absolute(distances), axis=-1)
-    return (distances[range(n_samples), patches] > 0).astype(np.int)
+    return np.asarray([d[np.argmax(np.abs(d), axis=-1)] > t for d in distances], dtype=int)
 
 
 def most_frequent(labels, distances, multi_class=True, t=0.0):
-    return np.array([np.argmax(np.bincount(patch_labels))
-                     for patch_labels in labels], copy=False)
+    return np.asarray([np.argmax(np.bincount(patch_labels)) for patch_labels in labels])
